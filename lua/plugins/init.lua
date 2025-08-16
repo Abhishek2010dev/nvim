e952ee5c9b0bdab9179ba3b615b62122326e4ac0
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-08-16 16:26:55 +0530

    feature: utils

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,19 @@
 return {
-	{'NMAC427/guess-indent.nvim'},
-
+	{ "NMAC427/guess-indent.nvim" },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+	{ -- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+	},
 }
